8713dc657165bb0dfb34727f832fbd352399af90
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-02 17:04:59 -0300

    Finish creating controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  get '/users/:user/posts', to: 'posts#index'
+  get '/users/:user/posts/:id', to: 'posts#show'
 end
