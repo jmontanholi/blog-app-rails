d562019400f34189034b820273a630e146d81606
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-08 17:53:30 -0300

    Remove ID from methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   belongs_to :user
   after_save :update_counter
  
-  def show_recent(id)
+  def show_recent
     comments.order(created_at: :desc).limit(5)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
 
-  def show_recent(id)
+  def show_recent
     posts.order(created_at: :desc).limit(3)
   end
 end
