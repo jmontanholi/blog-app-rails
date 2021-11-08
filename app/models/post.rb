871a41328bc1edf76bb4d85a3d747726d31a65e1
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-08 15:22:50 -0300

    Finish methods for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :user
+
+  def update_counter(id)
+    user = User.find_by(id: id)
+    user.posts_counter += 1
+    user.save
+  end
+
+  def show_recent(id)
+    Comment.where("post_id = ?", id).limit(5).order(created_at: :desc)
+  end
 end
