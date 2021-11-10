bd0231ebabcc7b7aeb69a6ec231832da27ed3d06
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-10 14:05:22 -0300

    Finish form to create a User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = user
     @posts = user.show_recent
   end
+
+  def new; end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      photo: params[:photo],
+      bio: params[:bio],
+      posts_counter: 0
+    )
+    if user.save
+      p 'Working'
+      flash[:notice] = 'User was successfully created'
+      redirect_to users_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'users#index'
 
+  get '/users/new', to: 'users#new'
+  post '/users/new', to: 'users#create'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   get '/users/:user/posts', to: 'posts#index'
   get '/users/:user/posts/:id', to: 'posts#show'
+  
 end
