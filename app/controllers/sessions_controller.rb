c229153d05f03fe06a282c9bc2097ed9c57831f2
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-10 14:49:31 -0300

    Finish login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    session[:user_id] = @user.id
+    p session[:user_id]
+    flash[:notice] = 'Successfully logged in'
+    redirect_to '/users'
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = 'Successfully logged out'
+    redirect_to '/users'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root 'users#index'
 
+  get 'sessions/new', to: 'sessions#new'
+  post 'sessions/new', to: 'sessions#create'
+  get 'sessions/logout', to: 'sessions#logout'
+  
   get '/users/new', to: 'users#new'
   post '/users/new', to: 'users#create'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
+
   get '/users/:user/posts', to: 'posts#index'
   get '/users/:user/posts/:id', to: 'posts#show'
-  
 end
