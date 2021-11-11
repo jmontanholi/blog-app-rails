bced23c490dddcfdf83f131f3b456b384dc28e98
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-11 15:01:50 -0300

    Finish tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, uniqueness: true
-  validates :name, :photo, presence: true
+  validates :name, :photo, :posts_counter, presence: true
   validates :name, :photo, :bio, length: { minimum: 4 }
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   has_many :comments
